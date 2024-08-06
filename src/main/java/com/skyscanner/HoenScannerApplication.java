9369136c19cd0b2f20bff196026a1ecc8ad7dd12
Hassan Mahmoud <hm253@st-andrews.ac.uk>
2024-08-07 00:19:37 +0100

    add run method to HoenScannerApplication class

--- src/main/java/com/skyscanner/HoenScannerApplication.java
+++ src/main/java/com/skyscanner/HoenScannerApplication.java
@@ -1,8 +1,14 @@
 package com.skyscanner;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.dropwizard.core.Application;
 import io.dropwizard.core.setup.Bootstrap;
 import io.dropwizard.core.setup.Environment;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import java.io.IOException;
 
 public class HoenScannerApplication extends Application<HoenScannerConfiguration> {
 
@@ -21,8 +27,23 @@ public class HoenScannerApplication extends Application<HoenScannerConfiguration
     }
 
     @Override
-    public void run(final HoenScannerConfiguration configuration, final Environment environment) {
-
+    public void run(final HoenScannerConfiguration configuration, final Environment environment) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        List<SearchResult> carResults = Arrays.asList(
+                mapper.readValue(
+                        getClass().getClassLoader().getResource("rental_cars.json"),
+                        SearchResult[].class
+                )
+        );
+        List<SearchResult> hotelResults = Arrays.asList(
+                mapper.readValue(
+                        getClass().getClassLoader().getResource("hotels.json"),
+                        SearchResult[].class
+                )
+        );
+        List<SearchResult> searchResults = new ArrayList<>();
+        searchResults.addAll(carResults);
+        searchResults.addAll(hotelResults);
     }
 
 }
